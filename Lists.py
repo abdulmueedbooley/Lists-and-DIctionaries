dc580e4485d2381de9521c7365e4bd20b6b8733b
abdulmueedbooley <abdulmueed.booley@gmail.com>
2026-08-18 10:07:08 +0200

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,9 @@
+# what is a list? 
+# A list stores multiple items in specific order
+
+#* EXAMPLE: SHOPPING LIST 
+shopping_list = ['milk', 'bread', 'eggs', 'apples']
+print(shopping_list) # THIS PRINTS OUT THE WHOLE LIST 
+print(shopping_list[0]) # THIS PRINTS OUT THE FIRST ITEM 
+print(shopping_list[1]) # THIS PRINTS OUT THE 2ND ITEM 
+
